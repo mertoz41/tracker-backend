e3aa19647d28353bb6045d3216d93d154543cc8c
mertoz41 <mertozka41@gmail.com>
2020-11-03 11:59:40 -0500

    registration complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def create
+        nu_user = User.create(username: params[:username], password: params[:password])
+        
+        if nu_user.valid?
+            render json: { user: nu_user}, status: :accepted
+        else
+            render json: { message: "Username already exists." }, status: :not_acceptable
+        end 
+
+    end 
 end
