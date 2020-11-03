d56d1b5dba117b606293702dee74a94c97e795d3
mertoz41 <mertozka41@gmail.com>
2020-11-03 13:09:19 -0500

    jwt and authentication implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    def encode(payload)
+        JWT.encode(payload, 'experiment', 'HS256')
+    end
+
+    def decode(token)
+        JWT.decode(token, 'experiment', true, {algorithm: 'HS256'})[0]
+    end 
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,13 +3,17 @@ class AuthController < ApplicationController
         user = User.find_by(username: params[:username])
 
         if user && user.authenticate(params[:password])
-            render json: {user: user}
+            payload = {user_id: user.id}
+            token = encode(payload)
+
+            render json: {user: user.as_json({:except => [:password_digest]}), token: token }
         else
             render json: {message: "Invalid username or password"}
         end 
+    end 
 
-
-        
-        
+    def check
+        user = User.find(decode(request.headers["Authentication"])["user_id"])
+        render json: {user: user.as_json({:except => [:password_digest]})}
     end 
 end
