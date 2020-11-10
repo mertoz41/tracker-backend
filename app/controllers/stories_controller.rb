50f69dcf33ac47d1dc292d0b4bfc0e1509e04173
mertoz41 <mertozka41@gmail.com>
2020-11-10 09:43:04 -0500

    deleting story is handled

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -3,4 +3,10 @@ class StoriesController < ApplicationController
         nu_story = Story.create(description: params[:description], project_id: params[:project_id], completed: false)
         render json: {new_story: nu_story.as_json(:include => :objectives)}
     end 
+
+    def destroy
+        story = Story.find(params[:id])
+        story.destroy
+        render json: {message: "story removed", deleted_story: story}
+    end 
 end
