636df25b9701ebfafcf4210ecb2dcfe9e42de4b3
mertoz41 <mertozka41@gmail.com>
2020-11-12 21:38:55 -0500

    in_progress property added to objective

---
+++ db/migrate/20201113023817_add_in_progress_to_objectives.rb
@@ -0,0 +1,5 @@
+class AddInProgressToObjectives < ActiveRecord::Migration[6.0]
+  def change
+    add_column :objectives, :in_progress, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_152428) do
+ActiveRecord::Schema.define(version: 2020_11_13_023817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_11_09_152428) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "story_id"
+    t.boolean "in_progress"
   end
 
   create_table "projects", force: :cascade do |t|
